3d2851190b52a5209437258a5603df372d9706ca
Joshua Pearson <mewurby@gmail.com>
2018-12-05 16:57:59 -0700

    Finalized

--- unit4/assignments/assignment44.cpp
+++ unit4/assignments/assignment44.cpp
@@ -24,170 +24,175 @@ float computeAverageBinary(int list[], int num);
 int linear(int list[], int num, int search);
 int binary(int list[], int num, int search);
 
-/**********************************************************************
-* main reads the list, determines the speed of the searches, and outputs
-* the results.
+/**********************************************************************                 
+* main reads the list, determines the speed of the searches, and outputs                
+* the results.                                                                          
 ***********************************************************************/
 int main()
 {
-   int list[1024];
-   const int MAX = sizeof (list) / sizeof (list[0]);
-   int num;
+	int list[1024];
+	const int MAX = sizeof (list) / sizeof (list[0]);
+	int num;
 
-   // read the numbers
-   if (!(num = readNumbers(list, MAX)))
-      return 1;
+	// read the numbers                                                                  
+	if (!(num = readNumbers(list, MAX)))
+		return 1;
 
-   // determine how long it takes for a linear search
-   float averageLinear;
-   averageLinear = computeAverageLinear(list, num);
+	// determine how long it takes for a linear search                                   
+	float averageLinear;
+	averageLinear = computeAverageLinear(list, num);
 
-   // determine how long it takes for a binary search
-   float averageBinary;
-   averageBinary = computeAverageBinary(list, num);
+	// determine how long it takes for a binary search                                   
+	float averageBinary;
+	averageBinary = computeAverageBinary(list, num);
 
-   // display results
-   cout.setf(ios::fixed);
-   cout.setf(ios::showpoint);
-   cout.precision(1);
-   cout << "Linear search: " << setw(10) << averageLinear << endl;
-   cout << "Binary search: " << setw(10) << averageBinary << endl;
+	// display results                                                                   
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(1);
+	cout << "Linear search: " << setw(10) << averageLinear << endl;
+	cout << "Binary search: " << setw(10) << averageBinary << endl;
 
-   return 0;
+	return 0;
 }
 
-/************************************************************
- * READ NUMBERS
- * Input:
- *    list:    a list of numbers to search through
- *    max:     the size of the numbers list
- * Output:
- *    num:     the number of items actually read
+/************************************************************                           
+ * READ NUMBERS                                                                         
+ * Input:                                                                               
+ *    list:    a list of numbers to search through                                      
+ *    max:     the size of the numbers list                                             
+ * Output:                                                                              
+ *    num:     the number of items actually read                                        
  ***********************************************************/
 int readNumbers(int list[], int max)
 {
-   char fileName[256];
-   int  num = 0;
+	char fileName[256];
+	int  num = 0;
 
-   // get the filename
-   cout << "Enter filename of list: ";
-   cin  >> fileName;
+	// get the filename                                                                  
+	cout << "Enter filename of list: ";
+	cin  >> fileName;
 
-   // open the file
-   ifstream fin(fileName);
-   if (fin.fail())
-   {
-      cout << "Unable to open file " << fileName << endl;
-      return 0;
-   }
+	// open the file                                                                     
+	ifstream fin(fileName);
+	if (fin.fail())
+	{
+		cout << "Unable to open file " << fileName << endl;
+		return 0;
+	}
 
-   // read the file
-   while (num < max && fin >> list[num])
-      num++;
+	// read the file                                                                     
+	while (num < max && fin >> list[num])
+		num++;
 
-   // make like a tree
-   fin.close();
-   return num;
+	// make like a tree                                                                  
+	fin.close();
+	return num;
 }
 
-/*********************************************************
- * COMPUTE AVERAGE LINEAR
- * Input:
- *    list:    a list of numbers to search through
- *    num:     the size of the numbers list
- * Output:
- *    averageLinear: the average number of comparisons it takes
- *                   to find each item in the array
+/*********************************************************                              
+ * COMPUTE AVERAGE LINEAR                                                               
+ * Input:                                                                               
+ *    list:    a list of numbers to search through                                      
+ *    num:     the size of the numbers list                                             
+ * Output:                                                                              
+ *    averageLinear: the average number of comparisons it takes                         
+ *                   to find each item in the array                                     
  *******************************************************/
 float computeAverageLinear(int list[], int num)
 {
-   float averageLinear = 0.0;
+	float averageLinear = 0.0;
 
-   for (int i = 0; i < num; i++)
+	for (int i = 0; i < num; i++)
 	{
 		averageLinear += linear(list, num, list[i]);
 	}
 	averageLinear = averageLinear / num;
-
-   return averageLinear;
+	return averageLinear;
 }
 
-/*********************************************************
- * COMPUTE AVERAGE BINARY
- * Input:
- *    list:    a list of numbers to search through
- *    num:    the size of the numbers list
- * Output:
- *    averageBinary: the average number of comparisons it takes
- *                   to find each item in the array
+/*********************************************************                              
+ * COMPUTE AVERAGE BINARY                                                               
+ * Input:                                                                               
+ *    list:    a list of numbers to search through                                      
+ *    num:    the size of the numbers list                                              
+ * Output:                                                                              
+ *    averageBinary: the average number of comparisons it takes                         
+ *                   to find each item in the array                                     
  *******************************************************/
 float computeAverageBinary(int list[], int num)
 {
-   float averageBinary = 0.0;
+	float averageBinary = 0.0;
 
-   for (int i = 0; i < num; i++)
+	for (int i = 0; i < num; i++)
 	{
 		averageBinary += binary(list, num, list[i]);
 	}
-	averageBinary = averageBinary / num; 
+	averageBinary = averageBinary / num;                                                            
 
-   return averageBinary;
+	return averageBinary;
 }
 
-/*******************************************************
- * LINEAR
- * Input:
- *    list:    a list of numbers to search through
- *    num:     the size of the numbers list
- *    search:  the number that you are searching for
- * Output:
- *    compares: the number of compares that were made
- *              before 'search' was found in the 'numbers' array
+/*******************************************************                                
+ * LINEAR                                                                               
+ * Input:                                                                               
+ *    list:    a list of numbers to search through                                      
+ *    num:     the size of the numbers list                                             
+ *    search:  the number that you are searching for                                    
+ * Output:                                                                              
+ *    compares: the number of compares that were made                                   
+ *              before 'search' was found in the 'numbers' array                        
  ******************************************************/
 int linear(int list[], int num, int search)
 {
-   bool found = false;
-   int  compares = 0;  // you will need to compute this
+	bool found = false;
+	int  compares = 0;  // you will need to compute this                                 
 
-   for (int i = 0; i < num && ! found; i++)
-      if (search == list[i])
-         found = true;
+	for (int i = 0; i < num && ! found; i++)
+	{
+		compares++;
+		if (search == list[i])
+		{
+			found = true;
+		}
+	}
 
-   return compares;
+	return compares;
 }
 
-/*******************************************************
- * BINARY
- * Input:
- *    list:    a list of numbers to search through
- *    num:     the size of the numbers list
- *    search:  the number that you are searching for
- * Output:
- *    compares: the number of compares that were made
- *              before 'search' was found in the 'numbers' array
+/*******************************************************                                
+ * BINARY                                                                               
+ * Input:                                                                               
+ *    list:    a list of numbers to search through                                      
+ *    num:     the size of the numbers list                                             
+ *    search:  the number that you are searching for                                    
+ * Output:                                                                              
+ *    compares: the number of compares that were made                                   
+ *              before 'search' was found in the 'numbers' array                        
  ******************************************************/
 int binary(int list[], int num, int search)
 {
-   bool found = false;
-   int  compares = 0;  // you will need to compute this
+	bool found = false;
+	int  compares = 0;  // you will need to compute this                                 
 
-   // set the bounds of the search space, initially the whole list
-   int iFirst = 0;
-   int iLast = num - 1;
+	// set the bounds of the search space, initially the whole list                      
+	int iFirst = 0;
+	int iLast = num - 1;
 
-   // continue until found or the search size is not zero
-   while (iLast >= iFirst && !found)
-   {
-      int iMiddle = (iLast + iFirst) / 2;
+	// continue until found or the search size is not zero                               
+	while (iLast >= iFirst && !found)
+	{
+		compares++;
+		int iMiddle = (iLast + iFirst) / 2;
 
-      // note that both the == and > count as one comparison
-      if (list[iMiddle] == search)
-         found = true;
-      else if (list[iMiddle] > search)
-         iLast = iMiddle - 1;
-      else
-         iFirst = iMiddle + 1;
-   }
+		// note that both the == and > count as one comparison                            
+		if (list[iMiddle] == search)
+			found = true;
+		else if (list[iMiddle] > search)
+			iLast = iMiddle - 1;
+		else
+			iFirst = iMiddle + 1;
+	}
 
-   return compares;
+	return compares;
 }
